5587c466d9598e2b28670f07fe290231e1cd93b4
Víctor Boix <victorboix@protonmail.com>
2020-04-10 13:00:15 +0200

    Settings page

--- Widget.js
+++ Widget.js
@@ -4,8 +4,6 @@ define(['dojo/_base/declare',
 ],
 function(declare, BaseWidget) {
   var clazz = declare([BaseWidget], {
-    templateString: '<div>This is a very simple widget. ' +
-    '<input type="button" value="Get Map Id" data-dojo-attach-event="click:_getMapId">.</div>',
 
     _getMapId: function(){
       alert(this.map.id);
---
+++ setting/Setting.js
@@ -0,0 +1,26 @@
+define([
+    'dojo/_base/declare',
+    'jimu/BaseWidgetSetting'
+  ],
+  function(declare, BaseWidgetSetting) {
+  
+    return declare([BaseWidgetSetting], {
+      baseClass: 'BaseWidgetSetting',
+  
+      postCreate: function(){
+        //the config object is passed in
+        this.setConfig(this.config);
+      },
+  
+      setConfig: function(config){
+        this.textNode.value = config.configText;
+      },
+  
+      getConfig: function(){
+        //WAB will get config object through this method
+        return {
+          configText: this.textNode.value
+        };
+      }
+    });
+  });
