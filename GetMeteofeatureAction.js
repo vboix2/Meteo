8d8154831c7ad01aed499769351dbd44e2bcc7aa
Víctor Boix <victorboix@protonmail.com>
2020-04-23 20:14:50 +0200

    valors nuls meteo

--- GetMeteofeatureAction.js
+++ GetMeteofeatureAction.js
@@ -18,34 +18,42 @@ define([
         .then(function(myWidget) {
           var dades = "";
           var metadades = "";
-          
-          featureSet.features.forEach(function(f){
-              if (f.attributes.YEAR<2009){
-                metadades = Strings.not_available_text;
-              } else {
-                var temperatura = f.attributes.TEMPERATUR;
-                var precipitacio = f.attributes.PRECIPITAC;
-                var humitat = f.attributes.HUMITAT;
-                var vent;
-                if (f.attributes.VENT==0){
-                  vent = " - ";
-                } else {
-                  vent = f.attributes.VENT;
-                }
+          var station;
 
-                var data = new Date(f.attributes.DATA);
-                metadades = Strings.text_station1 + f.attributes.ESTACIO_XE + Strings.text_station2 +
-                  data.getDate() + "/" + String(data.getMonth() + 1) + "/" + data.getFullYear();
+          featureSet.features.forEach(function (f) {
+            if (f.attributes.YEAR < 2009) {
+              metadades = Strings.not_available_text;
+            } else {
+              var temperatura = f.attributes.TEMPERATUR;
+              var precipitacio = f.attributes.PRECIPITAC;
+              var humitat = f.attributes.HUMITAT;
+              var vent = f.attributes.VENT;
+              station = f.attributes.ESTACIO_XE;
 
-                dades = "<table><tr><td>" + Strings.table_temp + ": </td><td> " + temperatura + "ºC </td></tr>" +
-                  "<tr><td>" + Strings.table_prec + ": </td><td> " + precipitacio + " mm </td></tr>" +
-                  "<tr><td>" + Strings.table_hum + ": </td><td> " + humitat + "% </td></tr>" +
-                  "<tr><td>" + Strings.table_wind + ": </td><td> " + vent + " m/s </td></tr>" +
-                  "</table>"
-              }
+              // null values
+              if (temperatura == 0) temperatura = " - ";
+              if (humitat == 0) humitat = " - ";
+              if (vent == 0) vent = " - ";
+
+              // weather data
+              dades = "<table><tr><td>" + Strings.table_temp + ": </td><td> " + temperatura + "ºC </td></tr>" +
+                "<tr><td>" + Strings.table_prec + ": </td><td> " + precipitacio + " mm </td></tr>" +
+                "<tr><td>" + Strings.table_hum + ": </td><td> " + humitat + "% </td></tr>" +
+                "<tr><td>" + Strings.table_wind + ": </td><td> " + vent + " m/s </td></tr>" +
+                "</table>"
+
+              // metadata
+              var data = new Date(f.attributes.DATA);
+              metadades = Strings.text_station1 + station + Strings.text_station2 +
+                data.getDate() + "/" + String(data.getMonth() + 1) + "/" + data.getFullYear();
+            }
           });
           myWidget.dadesMeteo.innerHTML = dades;
           myWidget.metadadesMeteo.innerHTML = metadades;
+
+          //var expression = "CODI = '" + String(station) + "'";
+          //myWidget.map.getLayer("XEMA_6372").setDefinitionExpression(expression);
+          //myWidget.map.getLayer("XEMA_6372").show();
         });
       }
   
